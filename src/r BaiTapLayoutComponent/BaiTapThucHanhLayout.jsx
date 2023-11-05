177558dd10502bcc6f7024d46f3441b1a934d867
tay52636 <phuongtay52636@gmail.com>
2023-11-05 16:23:33 +0700

    font icon up

--- src/r BaiTapLayoutComponent/BaiTapThucHanhLayout.jsx
+++ src/r BaiTapLayoutComponent/BaiTapThucHanhLayout.jsx
@@ -3,6 +3,7 @@ import Header from './Header'
 import './Style.css'
 import Body from './Body'
 import Footer from './Footer'
+import '../../node_modules/bootstrap-icons/font/bootstrap-icons.css'
 export default function BaiTapThucHanhLayout() {
   return (
     <div>
