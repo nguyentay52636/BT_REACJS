8e705474d519a92dfc613d08de02decaec5a9627
tay52636 <phuongtay52636@gmail.com>
2023-11-05 14:41:55 +0700

    update code buoi1

---
+++ src/r BaiTapLayoutComponent/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div>  
+      <footer class="py-5 bg-dark">
+    <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2023</p></div>
+</footer>
+</div>
+  )
+}
